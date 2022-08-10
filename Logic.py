fe5836b1d40ce24b3bfe9d9083afef2205cfd905
Kishore Kumar Ganta <76893429+kishorekumarganta@users.noreply.github.com>
2022-08-10 16:46:38 +0530

    Update Logic.py

--- Logic.py
+++ Logic.py
@@ -1,1 +1,8 @@
+# Ashmita Patel Strategy Logic
 
+# I am single, Unable to fetch Time Here, So Taking too Much Time for every release.
+
+# Bullish =Buy  Call 
+# Not Bullish = Sell Call
+# Bearish =  Buy Put 
+# Not Bearish =  Sell Put 
